4b4aa13f4b13c4abeb8e3100edd3e4c113de02f9
sirishacyd <sirishacyd@gmail.com>
2022-09-07 21:45:17 -0700

    Fix Indents

--- accounts/models.py
+++ accounts/models.py
@@ -60,24 +60,13 @@ class Account(AbstractBaseUser):
     
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
-
     objects = MyAccountManager()
 
-
     def __str__(self):
-            return self.email
-
+        return self.email
 
     def has_perm(self, perm, obj=None):
-            return self.is_admin
+        return self.is_admin
 
     def has_module_perms(self, add_label):
-            return True
-
-
-
-
-
-
-
-
+        return True
