d55a3aaed1a8c6a3c22c2a9bbc198d5856f8c83e
sirishacyd <sirishacyd@gmail.com>
2022-10-19 15:05:03 -0700

    user activation,forgot&Dashboard

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,8 +3,11 @@ from . import views
 
 urlpatterns=[
     path('register/',views.register, name='register'),
-     path('login/',views.login, name='login'),
-      path('logout/',views.logout, name='logout'),
-
-   
-]
+    
+    path('login/',views.login, name='login'),
+    path('logout/',views.logout, name='logout'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('activate/<uidb64>/<token>/', views.activate, name='activate'),
+    path('forgotPassword/', views.forgotPassword, name='forgotPassword'),
+    path('resetpassword_validate/<uidb64>/<token>/', views.resetpassword_validate, name='resetpassword_validate'),
+    path('resetPassword/', views.resetPassword, name='resetPassword'),]
