2681fa950f73840ed8656d021ec355d1b4d9233e
sirishacyd <sirishacyd@gmail.com>
2022-10-19 20:38:21 -0700

    commit

--- bookhouse/settings.py
+++ bookhouse/settings.py
@@ -152,6 +152,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # SMTP configuration
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'sirishacyd@gmail.com'
-EMAIL_HOST_PASSWORD = 'vyhcygekuwwjijuj'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
