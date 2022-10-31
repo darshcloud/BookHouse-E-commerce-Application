d2a22a3f24e06440a8fe9684a33be2cb057d8a30
Darshini Venkatesha Murthy Nag <darshini.vn@gmail.com>
2022-10-31 13:17:03 -0700

    Bookhouse After Order Functionalities

--- orders/admin.py
+++ orders/admin.py
@@ -1,8 +1,24 @@
 from django.contrib import admin
-
-# Register your models here.
 from .models import Payment, Order, OrderBook
 
+
+# Register your models here.
+
+class OrderBookInline(admin.TabularInline):
+    model = OrderBook
+    readonly_fields = ('payment', 'user', 'book', 'quantity', 'book_price', 'ordered')
+    extra = 0
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['order_number', 'full_name', 'phone', 'email', 'city', 'order_total', 'tax', 'status', 'is_ordered',
+                    'created_at']
+    list_filter = ['status', 'is_ordered']
+    search_fields = ['order_number', 'first_name', 'last_name', 'phone', 'email']
+    list_per_page = 20
+    inlines = [OrderBookInline]
+
+
 admin.site.register(Payment)
-admin.site.register(Order)
+admin.site.register(Order, OrderAdmin)
 admin.site.register(OrderBook)
