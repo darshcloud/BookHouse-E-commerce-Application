4a72ba2d74ea6e8eeeebdb65c139bd7ef82f21da
sirishacyd <sirishacyd@gmail.com>
2022-11-03 22:17:30 -0700

    User Profile Model

--- accounts/models.py
+++ accounts/models.py
@@ -37,12 +37,7 @@ class MyAccountManager(BaseUserManager):
         user.is_superadmin =True
         user.save(using=self._db)
         return user  
-             
-
-
-
-
-
+    
 class Account(AbstractBaseUser):
     first_name   = models.CharField(max_length=50)
     last_name    = models.CharField(max_length=50)
@@ -73,3 +68,18 @@ class Account(AbstractBaseUser):
 
     def has_module_perms(self, add_label):
         return True
+    
+class UserProfile(models.Model):
+    user = models.OneToOneField(Account, on_delete=models.CASCADE)
+    address_line_1 = models.CharField(blank=True, max_length=100)
+    address_line_2 = models.CharField(blank=True, max_length=100)
+    profile_picture = models.ImageField(blank=True, upload_to='userprofile')
+    city = models.CharField(blank=True, max_length=20)
+    state = models.CharField(blank=True, max_length=20)
+    country = models.CharField(blank=True, max_length=20)
+
+    def __str__(self):
+        return self.user.first_name
+
+    def full_address(self):
+        return f'{self.address_line_1} {self.address_line_2}'
