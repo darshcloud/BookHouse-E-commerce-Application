9eea023b7d6e8b47e8a3127fca869f363c7f5aae
sirishacyd <sirishacyd@gmail.com>
2022-11-03 20:51:06 -0700

    My Orders page

--- accounts/urls.py
+++ accounts/urls.py
@@ -10,4 +10,6 @@ urlpatterns=[
     path('activate/<uidb64>/<token>/', views.activate, name='activate'),
     path('forgotPassword/', views.forgotPassword, name='forgotPassword'),
     path('resetpassword_validate/<uidb64>/<token>/', views.resetpassword_validate, name='resetpassword_validate'),
-    path('resetPassword/', views.resetPassword, name='resetPassword'),]
+    path('resetPassword/', views.resetPassword, name='resetPassword'),
+    path('my_orders/', views.my_orders, name='my_orders'),
+]
