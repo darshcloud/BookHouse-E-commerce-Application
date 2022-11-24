12521588d13cd4c28738ba165ea6c74a7823be92
Bhavya Hegde <85700971+hegdebhavya@users.noreply.github.com>
2022-11-24 00:19:51 -0800

    RDS postgres Migration

--- bookhouse/settings.py
+++ bookhouse/settings.py
@@ -121,11 +121,25 @@ AUTH_USER_MODEL = 'accounts.Account'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+'default': {
+'ENGINE': 'django.db.backends.postgresql_psycopg2',
+'NAME': '',
+'USER': 'postgres',
+'PASSWORD': '',
+'HOST': 'bookhousedatabase.xxxxxxxxxxxxx.rds.amazonaws.com',
+'PORT': '5432',
 }
+}
+
+# DATABASES = {
+# 'default': {
+# 'ENGINE': 'django.db.backends.sqlite3',
+# 'NAME': BASE_DIR / 'db.sqlite3',
+
+# }
+# }
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
